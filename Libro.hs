539956211f084f298554f988a412c22dfd95859a
VillalobosGarciaGaby <gabriela.garcia.13355797@usalesiana.edu.bo>
2024-03-04 18:15:36 -0400

    Haskell

--- Libro.hs
+++ Libro.hs
@@ -1,4 +1,6 @@
 import Data.ByteString (group)
+import Data.Char
+
 
 -- Exercises 3.11
 -- 1. What are the types of the following values?
@@ -320,6 +322,121 @@ lastElement [x] = x
 lastElement (_:xs) = lastElement xs
 
 -}
+
+-- 1
+type Bit = Int
+
+unfold :: (t -> Bool) -> (t -> a) -> (t -> t) -> t -> [a]
+unfold p h t x
+  | p x = []
+  | otherwise = h x : unfold p h t (t x)
+
+int2bin :: Int -> [Bit]
+int2bin = unfold (== 0) (`mod` 2) (`div` 2)
+
+chop8 :: [Bit] -> [[Bit]]
+chop8 = unfold null (take 8) (drop 8)
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = unfold null (f . head) tail
+
+iterate' :: (a -> a) -> a -> [a]
+iterate' = unfold (const False) id
+
+-- 2
+
+type Bit = Int
+
+bin2int :: [Bit] -> Int
+bin2int bits = sum [w * b | (w, b) <- zip weights bits]
+  where
+    weights = iterate (* 2) 1
+
+bin2int' :: [Bit] -> Int
+bin2int' = foldr (\x y -> x + 2 * y) 0
+
+int2bin :: Int -> [Bit]
+int2bin 0 = []
+int2bin n = n `mod` 2 : int2bin (n `div` 2)
+
+make8 :: [Bit] -> [Bit]
+make8 bits = take 8 (bits ++ repeat 0)
+
+parity :: [Bit] -> Bit
+parity bits = sum bits `mod` 2
+
+addParity :: [Bit] -> [Bit]
+addParity bits = parity bits : bits
+
+encode :: String -> [Bit]
+encode = concatMap (addParity . make8 . int2bin . ord)
+
+chop9 :: [Bit] -> [[Bit]]
+chop9 [] = []
+chop9 bits = take 9 bits : chop9 (drop 9 bits)
+
+checkParity :: [Bit] -> [Bit]
+checkParity bits
+  | head bits == parity (tail bits) = tail bits
+  | otherwise = error "Parity Error"
+
+decode :: [Bit] -> String
+decode = map (chr . bin2int . checkParity) . chop9
+
+channel :: [Bit] -> [Bit]
+channel = id
+
+transmit :: String -> String
+transmit = decode . channel . encode
+
+-- 4
+
+altMap :: (a -> b) -> (a -> b) -> [a] -> [b]
+altMap _ _ [] = []
+altMap f0 f1 (x:xs) = f0 x : altMap f1 f0 xs
+
+-- 5
+import Data.List
+
+votes :: [String]
+votes = ["Red", "Blue", "Green", "Blue", "Blue", "Red"]
+
+count :: Eq a => a -> [a] -> Int
+count x = length . filter (== x)
+
+rmdups :: Eq a => [a] -> [a]
+rmdups [] = []
+rmdups (x:xs) = x : rmdups (filter (/= x) xs)
+
+result :: Ord a => [a] -> [(Int, a)]
+result vs = sort [(count v vs, v) | v <- rmdups vs]
+
+winner :: Ord a => [a] -> a
+winner = snd . last . result
+
+ballots :: [[String]]
+ballots =
+  [ ["Red", "Green"]
+  , ["Blue"]
+  , ["Green", "Red", "Blue"]
+  , ["Blue", "Green", "Red"]
+  , ["Green"]
+  ]
+
+rmemtpy :: Eq a => [[a]] -> [[a]]
+rmemtpy = filter (/= [])
+
+elim :: Eq a => a -> [[a]] -> [[a]]
+elim x = map (filter (/= x))
+
+rank :: Ord a => [[a]] -> [a]
+rank = map snd . result . map head
+
+winner' :: Ord a => [[a]] -> a
+winner' bs =
+  case rank (rmemtpy bs) of
+    [c] -> c
+    (c:cs) -> winner' (elim c bs)
 
 -- Ejercicio
 
